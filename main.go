9de60d44d43ef1e8595a5143e1f1231689fd1426
jwashin <washington.jim@gmail.com>
2020-06-24 13:28:29 -0400

    starting to get testing working

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter()
+	r := makeRouter()
+	http.Handle("/", r)
+}
+
+func makeRouter() *mux.Router {
+	r := mux.NewRouter()
+	app := r.Headers("X-Requested-With", "XMLHttpRequest").Subrouter()
+	app.HandleFunc("/user/{email}/{id}", restHandler).Methods(http.MethodGet)
+
+	return r
+}
+
+func restHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	email := params["email"]
+	fmt.Fprintf(w, email)
 }
